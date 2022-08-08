decda8c85117f5ec97cec5c1cba65da5e441c1fa
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-08-08 22:36:30 +0900

    [Beakjoon] [1931] 회의실 배정 (손으로 푸는 단계까지 완료)

---
+++ Beakjoon/src/회의실배정_1931.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class 회의실배정_1931 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		//회의의 수
+		int N = sc.nextInt();
+		//회의의 정보 입력받기
+		int[] num = new int[N];
+		for(int i =0;i<N;i++) {
+			int h= sc.nextInt();
+			int m=sc.nextInt();
+			//int[][] num = new int[T][]; 먼저 num[i][0] 가지고 정렬 후 num[i][1] 가지고 정렬
+			//k=0 , num[i][0] 이 더 크면 k에다가 num[i][1] 값 넣기 
+			// {{1,4} , {3,5}, {0,6} ...},  
+		
+		}
+	}
+
+}
