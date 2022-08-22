55d64403b0a7919937fd1fe3bf8ee73accb67826
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-08-22 23:16:40 +0900

    [BOJ] Solved 16953. A->B

---
+++ Beakjoon/src/BOJ_S2_16953_A_B.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_S2_16953_A_B {
+
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int A = Integer.parseInt(st.nextToken()); // 2
+		int B = Integer.parseInt(st.nextToken()); // 162
+		
+		int cnt = 1;
+		
+		//A와 B가 같아졌거나 아니면 B가 더 작아졌거나
+		while (A < B) {
+			if (B % 2 == 0)
+				B /= 2;
+			else if (B % 10 == 1)
+				B /= 10;
+			else
+				// 연산이 아무것도 안되면 나가버린다.
+				break;
+			cnt++;
+		}
+		// A와 B가 같다면 cnt
+		if (A == B)
+			System.out.println(cnt);
+		else
+			System.out.println(-1);
+
+	}
+}
