aafa57194ed05f56b79fb0588199d9b05a7d6ca1
Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-09-12 21:06:47 +0900

    [Silver I] Title: RGB거리, Time: 88 ms, Memory: 12116 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -3,29 +3,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		int[][] nums = new int[n][3];
-		
-		for (int i=0; i<n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j=0; j<3; j++) {
-				nums[i][j] = Integer.parseInt(st.nextToken());
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		int[][] arr = new int[T + 1][4];
+		int[][] dp = new int[T + 1][4];
+
+		for (int i = 1; i <= T; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= 3; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		for (int i=1; i<n; i++) {
-			nums[i][0] += Math.min(nums[i-1][1], nums[i-1][2]);
-			nums[i][1] += Math.min(nums[i-1][2], nums[i-1][0]);
-			nums[i][2] += Math.min(nums[i-1][0], nums[i-1][1]);
+
+
+		dp[1][1] = arr[1][1];
+		dp[1][2] = arr[1][2];
+		dp[1][3] = arr[1][3];
+
+		for (int i = 2; i <= T; i++) {
+			dp[i][1] = Math.min(dp[i - 1][2], dp[i - 1][3]) + arr[i][1];
+			dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][3]) + arr[i][2];
+			dp[i][3] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][3];
 		}
-		
-		
-		System.out.println(Math.min(nums[n-1][0], Math.min(nums[n-1][1], nums[n-1][2])));
+
+		int result = Math.min(dp[T][1], Math.min(dp[T][2], dp[T][3]));
+
+		System.out.println(result);
 	}
 }
