b8da17401e0b8a259e80224040fb43022dff0b84
Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-09-12 23:24:00 +0900

    [Silver I] Title: 포도주 시식, Time: 108 ms, Memory: 13080 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+ 
+		int[] DP = new int[N + 1];
+		int[] arr = new int[N + 1];
+ 
+ 
+		for (int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+ 
+		DP[1] = arr[1];
+		
+		// N 이 1이 입력될 수도 있음
+		if (N >= 2) {
+			DP[2] = arr[1] + arr[2];
+		}
+ 
+		for (int i = 3; i <= N; i++) {
+			DP[i] = Math.max(Math.max(DP[i - 2] , DP[i - 3] + arr[i - 1]) + arr[i],DP[i-1]);
+		}
+ 
+		System.out.println(DP[N]);
+ 
+	}
+ 
+}
