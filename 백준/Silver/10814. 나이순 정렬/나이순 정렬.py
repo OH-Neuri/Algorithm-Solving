263c2d2c9f0112bf157a0e9992921e468ceb7a3f
Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-11-20 23:09:10 +0900

    [Silver V] Title: 나이순 정렬, Time: 312 ms, Memory: 52636 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n= int(input())
+user=[]
+for _ in range(n):
+    user.append(list(input().split()))
+user.sort(key=lambda x: int(x[0]))
+
+for i in range(n):
+    print(user[i][0], user[i][1])
