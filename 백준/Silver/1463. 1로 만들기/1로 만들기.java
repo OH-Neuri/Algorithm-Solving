330059584a0a0789a2bd994cc05644c05db5e12b
Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-12-14 23:34:11 +0900

    [Silver III] Title: 1로 만들기, Time: 84 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+    System.out.println(recur(N, 0));
+  }
+
+  static int recur(int N, int count) {
+    // N이 2 미만인 경우 누적된 count값을 반환
+    if (N < 2) {
+      return count;
+    }
+    return Math.min(recur(N / 2, count + 1 + (N % 2)), recur(N / 3, count + 1 + (N % 3)));
+
+  }
+}
