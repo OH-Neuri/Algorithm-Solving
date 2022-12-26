071744713f3ee68d7ef5fa131217500f93df09b3
Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-12-27 01:06:54 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 636 ms, Memory: 144308 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,54 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	static boolean[] visited;
+	static ArrayList<Integer>[] A;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		A = new ArrayList[N + 1];
+		visited = new boolean[N + 1];
+
+		for (int i = 1; i < N + 1; i++) {
+			A[i] = new ArrayList<>();
+		}
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			A[s].add(e);
+			A[e].add(s);
+		}
+
+		int count = 0;
+		for (int i = 1; i < N + 1; i++) {
+			if (!visited[i]) {
+				count++;
+				DFS(i);
+			}
+		}
+		
+		System.out.println(count);
+	}
+
+	private static void DFS(int node) {
+		if (visited[node])
+			return;
+		visited[node] = true;
+		for (int x : A[node]) {
+			if (visited[x] == false)
+				DFS(x);
+		}
+	}
+}
