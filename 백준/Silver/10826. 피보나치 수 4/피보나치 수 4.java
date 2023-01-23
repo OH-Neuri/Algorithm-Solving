f48077ef6cb8fd2159962b80a55c76f6df4e85db
Neuri <87141845+OH-Neuri@users.noreply.github.com>
2023-01-23 22:11:05 +0900

    [Silver V] Title: 피보나치 수 4, Time: 120 ms, Memory: 18096 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        BigInteger[] dp = new BigInteger[10001];
+        dp[0] = new BigInteger("0");
+        dp[1] = new BigInteger("1");
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-2].add(dp[i-1]);
+        }
+        System.out.print(dp[n]);
+    }
+}
