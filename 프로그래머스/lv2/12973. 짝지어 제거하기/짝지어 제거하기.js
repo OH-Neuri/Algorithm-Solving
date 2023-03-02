69fc84be210d5146a8ed052c1b8be6d4fab6ea32
Neuri <87141845+OH-Neuri@users.noreply.github.com>
2023-03-02 14:56:23 +0900

    [level 2] Title: 짝지어 제거하기, Time: 26.12 ms, Memory: 46.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,9 @@
+function solution(s){
+    
+    const stack =[];
+    for(let i=0;i<s.length;i++){
+        if(!s.length||stack[stack.length-1] !==s[i]) stack.push(s[i])
+        else stack.pop();
+    }
+    return stack.length?0:1;
+}
