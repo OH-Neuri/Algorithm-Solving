bf963022167fb0628f1a8bbc9467d566c87f93c9
Neuri <87141845+OH-Neuri@users.noreply.github.com>
2023-03-04 20:28:25 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,8 +1,4 @@
 function solution(n) {
-    let cnt=0;
-    for(let i=1;i*i<n;i++){
-        if(n%i==0) cnt++;
-    }
-    console.log(Math.sqrt(n))
-    return (Math.sqrt(n))%1==0?cnt*2+1:cnt*2;
+    var answer = 0;
+    return answer;
 }
