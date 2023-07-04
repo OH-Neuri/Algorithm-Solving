67fdc4ed2fe7c1b0ec4cd8615e86577cc9dac5fe
HaNeul <moho000498@gmail.com>
2023-07-04 22:52:37 +0900

    [Gold II] Title: 트리의 지름, Time: 544 ms, Memory: 71492 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -1,39 +1,26 @@
 import sys
+sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 9)
-
-N = int(input())
 
 def DFS(node):
-    for a, b in graph[node]:
+    for a, b in tree[node]:
         if visited[a] == -1:
             visited[a] = visited[node] + b
-            if _max[1] < visited[a]:
-                _max[1] = visited[a]
-                _max[0] = a
             DFS(a)
 
-graph = [[] for _ in range(N+1)]
-
-
+N = int(input())
+tree = [[] for _ in range(N+1)]
 for _ in range(N):
     c = list(map(int, input().split()))
     for e in range(1, len(c) - 2, 2):
-        graph[c[0]].append((c[e], c[e + 1]))
+        tree[c[0]].append((c[e], c[e + 1]))
 
 visited = [-1 for _ in range(N+1)]
-_max = [0,0]
 visited[1] = 0
 DFS(1)
+max_node = visited.index(max(visited))
 
-start = _max[0]
 visited = [-1 for _ in range(N+1)]
-_max = [0,0]
-visited[start] = 0
-DFS(start)
-print(_max[1])
-
-
-
-
-
+visited[max_node] = 0
+DFS(max_node)
+print(max(visited))
