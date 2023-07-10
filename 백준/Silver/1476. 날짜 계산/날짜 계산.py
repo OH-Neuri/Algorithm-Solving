a2c65acc20b79b68e5a9e0a4638e2f5a065c382e
HaNeul <moho000498@gmail.com>
2023-07-10 23:21:59 +0900

    [Silver V] Title: 날짜 계산, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -0,0 +1,11 @@
+E, S, M = map(int,input().split())
+e, s, m = 1,1,1
+result = 1
+while True:
+    if e==E and s==S and m==M:
+        break
+    e+=1; s+=1; m+=1; result+=1;
+    if e>=16 : e=1
+    if s>=29 : s=1
+    if m>=20 : m=1
+print(result)
