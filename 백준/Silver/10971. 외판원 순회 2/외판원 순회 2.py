34437c83f8b0f9cb7d790af997c30e0d99688849
HaNeul <moho000498@gmail.com>
2023-07-12 21:51:00 +0900

    [Silver II] Title: 외판원 순회 2, Time: 540 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -3,26 +3,19 @@ input = sys.stdin.readline
 
 N = int(input())
 travel_cost = [list(map(int,input().split(" "))) for _ in range(N)]
-visited_index = []
 visited = [0] * N
 m = 1e9
 
-def dfs(depth, start, init):
+def dfs(depth, start, cost):
     global m
-    if depth == N and travel_cost[start][init]!=0:
-        m = min(m,sum(visited_index)+travel_cost[start][init])
+    if depth == N-1 and travel_cost[start][0]!=0:
+        m = min(m,cost+travel_cost[start][0])
         return
     for i in range(N):
         if not visited[i] and travel_cost[start][i]!=0:
             visited[i]=1
-            visited_index.append(travel_cost[start][i])
-            dfs(depth+1, i, init)
-            # 다음 i 가기 전 초기화
-            visited_index.pop()
+            dfs(depth+1, i, cost + travel_cost[start][i])
             visited[i]=0
-for i in range(N):
-    visited[i]=1
-    dfs(1,i,i)
-    # 다음 i 가기 전 초기화
-    visited[i]=0
+visited[0]=1
+dfs(0,0,0)
 print(m)
