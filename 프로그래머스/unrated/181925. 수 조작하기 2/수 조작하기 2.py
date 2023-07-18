bf0eee89bd1a82bd5de764f19147d89c22bfc3fa
HaNeul <moho000498@gmail.com>
2023-07-18 23:40:42 +0900

    [unrated] Title: 수 조작하기 2, Time: 35.23 ms, Memory: 12.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,13 @@
+def solution(numLog):
+    answer = ""
+    for i in range(1,len(numLog)):
+        if numLog[i]-numLog[i-1]==1:
+            answer+="w"
+        elif numLog[i]-numLog[i-1]==-1:
+            answer+="s"
+        elif numLog[i]-numLog[i-1]==10:
+            answer+="d"
+        elif numLog[i]-numLog[i-1]==-10:
+            answer+="a"
+            
+    return answer
