9318d180b9c5e47e6444fa07d8ebace2378f3cbb
HaNeul <moho000498@gmail.com>
2023-08-10 23:20:03 +0900

    [level 2] Title: 더 맵게, Time: 1617.51 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -1,13 +1,20 @@
-from heapq import *
-
+import heapq
 def solution(scoville, K):
-    cnt=0
-    heapify(scoville)
-    while scoville[0]<K and len(scoville)>1:
-        sco1 = heappop(scoville)
-        sco2 = heappop(scoville)
-        heappush(scoville, sco1+sco2*2)
-        cnt+=1
-    return cnt if scoville[0]>=K else -1 
-    
+    # 1. 최소힙 만든다
+    # 2. 배열의 최솟값이 K이상 될때까지 while 
+    # 3. 배열 스코빌 계산 작업
+    # 4. cnt 체크
+    heapq.heapify(scoville)
+    answer = 0
+    while scoville[0]<K and 2<=len(scoville) :
+        answer += 1
+        scoville_1 = heapq.heappop(scoville)
+        scoville_2 = heapq.heappop(scoville)
+        heapq.heappush(scoville,(scoville_1+(scoville_2*2)))
+        
+    if scoville[0]>=K:
+        return answer
+    else: return -1
+
+print(solution([0,1],2))
         
