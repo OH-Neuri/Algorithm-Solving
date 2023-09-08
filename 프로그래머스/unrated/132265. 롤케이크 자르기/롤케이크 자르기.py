7d2533bca67d21b3e180dc8947e5b38045277ee2
HaNeul <moho000498@gmail.com>
2023-09-08 22:55:14 +0900

    [unrated] Title: 롤케이크 자르기, Time: 665.31 ms, Memory: 51.1 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,17 +1,32 @@
+# from collections import Counter
+# def solution(topping):
+#     answer = 0
+#     a = Counter(topping)
+#     b = {}
+#     for i in range(len(topping)):
+#         if topping[i] in a.keys():
+#             a[topping[i]] -=1
+#             if a[topping[i]] ==0:
+#                 del a[topping[i]]
+#             if topping[i] in b.keys():
+#                 b[topping[i]]+=1
+#             else:
+#                 b[topping[i]]=1
+#         if len(a)==len(b):
+#             answer+=1
+#     return answer
 from collections import Counter
 def solution(topping):
     answer = 0
     a = Counter(topping)
-    b = {}
-    for i in range(len(topping)):
-        if topping[i] in a.keys():
-            a[topping[i]] -=1
-            if a[topping[i]] ==0:
-                del a[topping[i]]
-            if topping[i] in b.keys():
-                b[topping[i]]+=1
-            else:
-                b[topping[i]]=1
+    b = set()
+    for x in topping:
+        a[x]-=1
+        b.add(x)
+        
+        if a[x]==0:
+            del a[x]
+        
         if len(a)==len(b):
             answer+=1
     return answer
