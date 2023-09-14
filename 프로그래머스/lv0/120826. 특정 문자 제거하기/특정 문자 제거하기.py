697b1606c7bf00f08e8a2cea6a5e65eff580dcab
HaNeul <moho000498@gmail.com>
2023-09-14 21:23:39 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -0,0 +1,10 @@
+def solution(my_string, letter):
+    # answer = ''
+    # print(my_string.replace(letter,""))
+    # # while True:
+    # #     if letter in my_string:
+    # #         my_string.replace(letter,"")
+    # #     else:
+    # #         break
+    # print(my_string)
+    return my_string.replace(letter,"")
