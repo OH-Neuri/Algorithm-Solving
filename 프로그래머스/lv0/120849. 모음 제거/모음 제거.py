2511b3c6ac6bf596b908e9f367f5d785838836a9
HaNeul <moho000498@gmail.com>
2023-09-19 22:54:59 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
@@ -1,7 +1,2 @@
 def solution(my_string):
-    vowel={'a':'a','e':'e','u':'u','o':'o','i':'i'}
-    answer = ''
-    for string in my_string:
-        if string not in vowel.keys():
-            answer+=string
-    return answer
+    return "".join([i for i in my_string if not(i in "aeiou")])
