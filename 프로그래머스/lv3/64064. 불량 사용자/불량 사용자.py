f6c2149b49be4984de08c7eafe98ec2cb5f8088b
HaNeul <moho000498@gmail.com>
2023-09-22 00:06:19 +0900

    [level 3] Title: 불량 사용자, Time: 5.86 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
@@ -0,0 +1,26 @@
+from itertools import permutations
+    
+def check(user, ban):
+    if len(user) != len(ban):
+        return False
+    else:
+        for i, j in zip(user, ban):
+            if j == '*':
+                continue
+            if i != j:
+                return False
+        return True
+        
+def solution(user_id, banned_id):
+    answer = []
+    for i in permutations(user_id, len(banned_id)):
+        count = 0
+        for a, b in zip(i, banned_id):
+            if check(a, b):
+                count += 1
+                
+        if count == len(banned_id):
+            if set(i) not in answer:
+                answer.append(set(i))
+    
+    return len(answer)
