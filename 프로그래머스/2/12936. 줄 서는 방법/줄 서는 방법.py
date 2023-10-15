d414a6cc36601eb030c4c01c0254003c714cdd73
HaNeul <moho000498@gmail.com>
2023-10-15 14:12:25 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -0,0 +1,18 @@
+import math
+
+def solution(n, k):
+    answer = [i for i in range(1, n + 1)]
+    stack = []
+    k = k-1
+
+    while answer:
+        # 몫이 맨 첫번째 자리
+        a = k // math.factorial(n - 1)
+        stack.append(answer[a])
+        del answer[a]
+
+        # k를 줄여야함
+        k = k % math.factorial(n - 1)
+        n -= 1
+
+    return stack
