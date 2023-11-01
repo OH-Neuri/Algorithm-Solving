bbba1062e676a8d107bd180350baf3b95e8c27a9
HaNeul <moho000498@gmail.com>
2023-11-01 21:29:03 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,27 +1,18 @@
 def solution(s):
-    # 구간이 겹치면 압축한다 -> 문자열 길이에서 구간 값 뺴기
-    # 각 단위별 문자열 길이는 dict 에 저장한다.
-    l = len(s)
-    if l == 1:
-        return 1 
-    str_length = {}
-    for i in range(1,l//2+1):
-        s_len = l
-        cnt= 0
-        flag = False
-        for j in range(0,l,i):
-            if s[j:j+i]==s[j+i:j+i+i]:
-                s_len -= i
-                if not flag:
-                    flag = True
-                if flag:
-                    cnt+=1
-                    print(s[j:j+i],s[j+i:j+i+i],cnt)
+    candi = [i for i in range(1, (len(s)//2+1))]
+    answer = len(s)
+    
+    for length in candi:
+        key = s[:length]
+        shorten = []
+        cnt = 1
+        for  i in range(length, len(s), length):
+            if key == s[i:i+length]:
+                cnt += 1
             else:
-                flag = False
-                if cnt>0:
-                    s_len += len(str(cnt+1))
-                cnt=0
-        str_length[i] = s_len+cnt
-    print(str_length)
-    return min(str_length.values())
+                shorten.append([key, cnt])
+                cnt = 1
+                key = s[i:i+length]
+        shorten.append([key, cnt])
+        answer = min(answer, sum([len(key) + (len(str(cnt)) if cnt > 1 else 0) for key, cnt in shorten]))
+    return answer
