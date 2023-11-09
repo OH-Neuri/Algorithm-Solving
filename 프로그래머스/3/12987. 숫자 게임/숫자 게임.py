eab19a5431685f9b5c41dff0506441ee7a32651a
HaNeul <moho000498@gmail.com>
2023-11-09 20:55:24 +0900

    [level 3] Title: 숫자 게임, Time: 45.70 ms, Memory: 19 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -0,0 +1,18 @@
+from collections import deque
+def solution(A, B):
+    answer = 0
+    
+    A.sort()
+    B.sort()
+    A = deque(A)
+    B = deque(B)
+    
+    while B:
+        if A[0]<B[0]:
+            answer+=1
+            A.popleft()
+            B.popleft()
+        else:
+            B.popleft()
+            
+    return answer
