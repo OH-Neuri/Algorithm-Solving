686a5b7abe9349de877249813ae6f73428e024a7
HaNeul <moho000498@gmail.com>
2023-12-07 00:31:04 +0900

    [Gold IV] Title: 불!, Time: 1276 ms, Memory: 55228 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -1,67 +1,74 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-def f_bfs():
-    while f_deq:
-        x,y = f_deq.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            # graph 벗어난 좌표는 무시
-            if nx < 0 or nx > (r-1) or ny < 0 or ny > (c-1): 
-                continue
-            '''
-            'J'에 해당하는 좌표는 0으로 미리 바꿔뒀기 때문에
-            graph[nx][ny] == '.'일 때만 불이 이동할 수 있도록 함.
-            '''
-            if graph[nx][ny] == '.':
-                graph[nx][ny] = graph[x][y] + 1
-                f_deq.append([nx,ny])
+# 시계방향
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
 
-def j_bfs():
-    while j_deq:
-        x,y = j_deq.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            # graph를 벗어나면 탈출했음을 의미한다.
-            if nx < 0 or nx > (r-1) or ny < 0 or ny > (c-1):
-                time = graph[x][y] + 1
-                return print(time)  # 탈출!
-             #불이 번지지 않았으면 해당 좌표값이 '.'인 경우도 있음을 고려
-            if graph[nx][ny] == '.':
-                graph[nx][ny] = graph[x][y] + 1
-                j_deq.append([nx,ny])
-            # 해당 좌표값이 '#'이면 밑의 if문이 오류가 나서 걸러주기 위해 추가
-            elif graph[nx][ny] != '#':
-                if graph[nx][ny] > (graph[x][y] + 1): # 불의 값이 지훈이보다 크면
-                    graph[nx][ny] = graph[x][y] + 1
-                    j_deq.append([nx,ny])
-    time = 'IMPOSSIBLE' # 지훈이가 탈출하지 못하면 이 문장에 도달   
-    return print(time)
+R, C = map(int,input().split())
+arr = [] # 미로
+J_x, J_y = 0, 0  # 지훈이 위치
+q = deque() # 불난곳
+j_q = deque()  # 지훈이가 갈 수 있는 경로
+fire_visited = [[False]*C for _ in range(R)] # 불난곳 방문처리
+j_visited = [[False]*C for _ in range(R)] # 지훈녀석 방문처리
+for i in range(R):
+    data = list(input().rstrip())
+    arr.append(data)
+    for idx, j in enumerate(data):
+        if j == 'J':
+            j_visited[i][idx] = True
+            j_q.append((i,idx))
+        if j == "F":
+            fire_visited[i][idx] = True
+            q.append((i,idx))
 
-# r: 행(x), c: 열(y)
-r,c = map(int,input().split())
 
-# graph
-graph = []
-for i in range(r):
-    graph.append(list(input()))
+time = 1
+def escape():
+    global time
 
-#deque
-j_deq = deque()
-f_deq = deque()
-dx = [0,0,-1,1]
-dy = [-1,1,0,0]
+    while True:
+        
+        # 불이 퍼진다
+        fire_tmp = []
+        while q:
+            x, y = q.popleft()
 
-# 지훈, 불 처음 위치 큐에 넣기
-for i in range(r):
-    for j in range(c):
-        if graph[i][j] == 'F':
-            f_deq.append([i,j])
-            graph[i][j] = 0 # 0부터 시작해서 해당 위치 시간 파악
-        if graph[i][j] == 'J':
-            j_deq.append([i,j])
-            graph[i][j]= 0  # 0부터 시작해서 해당 위치 시간 파악
+            for k in range(4):
+                nx = x + dx[k]
+                ny = y + dy[k]
 
-f_bfs() # 불 먼저 bfs돌려서 언제 어디까지 퍼지는지 graph에 표기
-j_bfs()
+                if 0<=nx<R and 0<=ny<C and not fire_visited[nx][ny] and arr[nx][ny]!='#':
+                    fire_visited[nx][ny] = True
+                    fire_tmp.append((nx,ny))
+        q.extend(fire_tmp)
+        
+        # 지훈이가 이동한다
+        j_tmp = []
+        while j_q:
+            j_x, j_y = j_q.popleft()
+
+            for k in range(4):
+                nx = j_x + dx[k]
+                ny = j_y + dy[k]
+
+                # 배열 밖이면 탈출
+                if 0>nx or nx>=R or 0>ny or ny>=C:
+                    return True
+                # 아직 배열 안에 있을 경우
+                if arr[nx][ny]!='#' and not fire_visited[nx][ny] and not j_visited[nx][ny]:
+                    j_visited[nx][ny] = True
+                    j_tmp.append((nx,ny))
+        j_q.extend(j_tmp)
+        
+        # 지훈이가 갈 수 있는 경로가 없을 경우 실패
+        if len(j_tmp)==0:
+            return False
+        time+=1
+
+if escape():
+    print(time)
+else:
+    print('IMPOSSIBLE')
