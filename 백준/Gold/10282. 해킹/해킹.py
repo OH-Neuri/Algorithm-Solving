e2f4086917b547ed5549c391833a6bc5b629b177
HaNeul <moho000498@gmail.com>
2023-12-20 23:47:00 +0900

    [Gold IV] Title: 해킹, Time: 792 ms, Memory: 49220 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.py
+++ 백준/Gold/10282. 해킹/해킹.py
@@ -1,9 +1,6 @@
-import heapq
+from heapq import heappush, heappop
 import sys
 input = sys.stdin.readline
-
-# 감염된 컴퓨터 => visited True 개수
-# 가중치 갱신
 
 tc = int(input())
 for _ in range(tc):
@@ -11,35 +8,25 @@ for _ in range(tc):
     # 컴퓨터 개수, 의존성 개수, 해킹당한 컴퓨터 번호
     n, d, c = map(int,input().split())
     graph = [[] for _ in range(n+1)]
-    dist = [float('inf') for _ in range(n+1)]
-
+    dist = [float('inf')]*(n+1)
+    visited = [False]*(n+1)
     for _ in range(d):
         s, e, v = map(int,input().split())
         graph[e].append((s,v))
 
-    def dijkstra(start):  # start로 부터의 거리 값을 저장하기 위함
-        q = []
-        dist[c] = 0  # 시작 값은 0
-        heapq.heappush(q, [dist[start],start])
 
-        while q:
-            curr_dist, curr_dest = heapq.heappop(q)
+    heap= [(0,c)]
+    dist[c] = 0  # 시작 값은 0
+    while heap:
+        start_dist, start_node = heappop(heap)
+        if visited[start_node]:
+            continue
+        visited[start_node] = True
+        time = start_dist
 
-            if dist[curr_dest] < curr_dist: # 기존에 있는 거리보다 길다면, 볼 필요 없음
-                continue
+        for next_node, next_dist in graph[start_node]:
+            if start_dist + next_dist < dist[next_node]:
+                dist[next_node] = start_dist + next_dist
+                heappush(heap, (start_dist+next_dist, next_node))
 
-            for new_dest, new_dist in graph[curr_dest]:
-                distance = curr_dist + new_dist # 해당 노드를 거쳐 갈 때 거리
-                if distance < dist[new_dest]: # 알고 있는 거리보다 작으면 갱신
-                    dist[new_dest] = distance
-                    heapq.heappush(q, [distance, new_dest]) # 다음 인접 거리를 계산하기 위해 큐에 삽입
-
-    dijkstra(c)
-
-    com_cnt = 0
-    com_time = 0
-    for i in dist:
-        if i != float('inf'):
-            com_time = max(com_time,i)
-            com_cnt +=1
-    print(com_cnt, com_time)
+    print(sum(visited), time)
