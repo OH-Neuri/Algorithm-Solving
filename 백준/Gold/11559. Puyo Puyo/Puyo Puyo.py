b49eabc580130339380ac0e389bac607a53c7b3c
HaNeul <moho000498@gmail.com>
2024-01-29 23:38:21 +0900

    [Gold IV] Title: Puyo Puyo, Time: 64 ms, Memory: 34148 KB -BaekjoonHub

---
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
@@ -0,0 +1,60 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+field = [list(map(str, input().rstrip())) for _ in range(12)]
+
+# c : 색깔
+# 터질 뿌요들 모아오기
+def BFS(i, j, c):
+    visited[i][j] = True
+    q = deque()
+    q.append((i, j))
+    e = [(i,j)]
+    while q:
+        x, y = q.popleft()
+        for k in range(4):
+            nx, ny = x + dx[k], y + dy[k]
+            if 0<=nx<12 and 0<=ny<6 and not visited[nx][ny] and field[nx][ny] == c:
+                visited[nx][ny] = True
+                q.append((nx,ny))
+                e.append((nx,ny))
+    if len(e) >=4:
+        return e
+    else:
+        return []
+
+# 터뜨린 다음 뿌요들 아래로 내리기
+# e : 터질 뿌요들
+def explode_puyo(e):
+    # 터뜨리기
+    for i in range(len(e)):
+        field[e[i][0]][e[i][1]] = '.'
+    for i in range(6):
+        idx = -1
+        for j in range(11,-1,-1):
+            if idx != -1 and field[j][i] != '.':
+                field[idx][i] = field[j][i] # 내려오기
+                field[j][i] = '.'
+                idx -= 1
+            if idx == -1 and field[j][i] == '.': # 맨 처음 빈공간 찾기
+                idx = j
+
+ep = 0
+while True:
+    ep_list = [] # 1연쇄에 터질 뿌요들
+    visited = [[False]*6 for _ in range(12)]
+    for i in range(12):
+        for j in range(6):
+            if field[i][j] != '.' and not visited[i][j]:
+                ep_list += BFS(i,j,field[i][j])
+    if len(ep_list)>=4:
+        explode_puyo(ep_list)
+        ep += 1
+    else:
+        break
+
+print(ep)
