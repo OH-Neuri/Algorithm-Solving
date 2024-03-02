6ee238282e4bc98bd1596b4f9416dd4d0fb243e3
HaNeul <moho000498@gmail.com>
2024-03-02 23:43:10 +0900

    [Silver III] Title: 모든 순열, Time: 200 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -0,0 +1,14 @@
+n = int(input())
+temp = []
+
+def dfs():
+    if len(temp) == n:
+        print(*temp)
+        return
+    for i in range(1, n + 1):
+        if i not in temp:
+            temp.append(i)
+            dfs()
+            temp.pop()
+
+dfs()
