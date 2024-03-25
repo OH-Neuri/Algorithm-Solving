cd9571fd45d7a5737458f7c64e17acd2bd37f383
HaNeul <moho000498@gmail.com>
2024-03-25 23:46:36 +0900

    [level 1] Title: 대충 만든 자판, Time: 8.12 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,4 +1,23 @@
 function solution(keymap, targets) {
+    let hash = {}
     var answer = [];
+    targets.forEach(t=>{
+        let sum = 0 
+        for(let i=0;i<t.length;i++){
+            let min = Infinity
+            for(let j=0; j<keymap.length; j++){
+                let index = keymap[j].indexOf(t[i])
+                if(min >index && index!=-1) min = index
+            }
+            if(min==Infinity){
+                sum = -1
+                break
+            }
+            else {
+                sum += min+1
+            }
+        }
+        answer.push(sum)
+    })
     return answer;
 }
