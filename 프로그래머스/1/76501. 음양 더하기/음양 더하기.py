a48240e1dcbac3b82b773f373dcc8f1c196acfd6
HaNeul <moho000498@gmail.com>
2024-04-02 22:56:26 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,8 @@
+def solution(absolutes, signs):
+    answer = 0
+    for a, s in zip(absolutes, signs):
+        if s:
+            answer +=a
+        else:
+            answer -=a
+    return answer
