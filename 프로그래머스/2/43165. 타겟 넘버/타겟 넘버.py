526df36f41af4f461222f7d60fca282e9ad30b10
HaNeul <moho000498@gmail.com>
2024-04-07 22:44:16 +0900

    [level 2] Title: 타겟 넘버, Time: 4.68 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,18 +1,7 @@
 def solution(numbers, target):
-    global answer
-    answer = 0
-        
-    def DFS(depth, t, summ):
-        global answer
-        
-        if depth == len(numbers):
-            if summ == t:
-                answer +=1
-            return 
-
-        DFS(depth+1, t, summ + numbers[depth])
-        DFS(depth+1, t, summ - numbers[depth])
-
-    DFS(0,target,0)
-    
-    return answer
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
