4e0b896119138250db038685578f1571407e2d87
HaNeul <moho000498@gmail.com>
2024-04-10 00:23:09 +0900

    [level 2] Title: 호텔 대실, Time: 2.06 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,24 +1,24 @@
 from heapq import heappop, heappush
 def solution(book_time):
-    idx = 0
-    answer = 1
-    for start_time, end_time in book_time:
-        start = int(start_time[:2])*60 + int(start_time[3:])
-        end = int(end_time[:2])*60 + int(end_time[3:])
-        book_time[idx][0] = start
-        book_time[idx][1] = end
-        idx+=1
-    book_time.sort()
-    
     heap = []
-    for s, e in book_time:
-        if not heap:
-            heappush(heap,e)
-            continue
-        if heap[0] <= s:
-            heappop(heap)
-        else:
-            answer += 1
-        heappush(heap,e+10)
+    book_time = [[time_to_minutes(start), time_to_minutes(end)] for start, end in book_time]
+    book_time = sorted(book_time, key=lambda x: x[0])
+    answer = 0
     
+    for start, end in book_time:
+        if not heap:
+            heappush(heap, end+10)
+            answer +=1
+            continue
+            
+        early_time = heappop(heap)
+        if early_time > start:
+            answer +=1
+            heappush(heap,early_time)
+        heappush(heap,end+10)
+        
     return answer
+
+def time_to_minutes(time_str):
+    hour, minute = map(int, time_str.split(':'))
+    return hour * 60 + minute
