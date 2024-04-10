1bc9356daf1b46718b41ee5846d4237747d85df2
HaNeul <moho000498@gmail.com>
2024-04-10 14:45:18 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 2.39 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/README.md
+++ 프로그래머스/2/70129. 이진 변환 반복하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이진 변환 반복하기 - 70129 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 1.53 ms
+메모리: 10.4 MB, 시간: 2.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 18:32:37
+2024년 04월 10일 14:45:17
 
 ### 문제 설명
 
--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,17 +1,8 @@
 def solution(s):
-    answer = []
-    zero_cnt = 0
-    work_cnt = 0
-    l = 0
-
-    while s != '1':
-        # 제거할 0의 개수
-        zero_cnt += s.count('0')
-        # 0 제거 후 길이
-        l = len(s.replace('0',''))
-        # 이진 변환 결과
-        s = bin(l)[2:]
-        work_cnt +=1
-        
-    return [work_cnt, zero_cnt]
-
+    change_cnt = 0
+    zero_cnt = 0     
+    while s!='1':
+        zero_cnt += s.count("0")    
+        s = bin(len(s.replace("0","")))[2:]
+        change_cnt +=1
+    return [change_cnt,zero_cnt]
