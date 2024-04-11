9c2de6ed54a2ab82d7ca3f700bd48fa99675e2db
HaNeul <moho000498@gmail.com>
2024-04-11 22:30:04 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,25 +1,17 @@
-from collections import deque
 def solution(progresses, speeds):
-    q = deque(progresses)
-    s = deque(speeds)
     day = 1
     distribute = 0
     answer = []
-    
-    while q:
-        c_q = q.popleft()
-        c_s = s.popleft()
-        
-        if c_q + (c_s * day) < 100:
+    for p, s in zip(progresses,speeds):
+        if p + (s * day) < 100:
             if distribute:
                 answer.append(distribute)
                 distribute = 0
-            while c_q + (c_s * day) <100:
+            while p + (s * day) <100:
                 day+=1
             distribute = 1
         else:
             distribute +=1
-        
     answer.append(distribute)
     
     return answer
