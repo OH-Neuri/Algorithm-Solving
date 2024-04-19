9772839cb6995df4e167cf887d278856e142464d
HaNeul <moho000498@gmail.com>
2024-04-19 23:27:50 +0900

    [level 2] Title: 예상 대진표, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,8 +1,6 @@
 def solution(n,a,b):
-    round = 0
+    answer = 0
     while a!=b:
-        a = (a+1)//2
-        b = (b+1)//2
-        round +=1
-
-    return round
+        answer +=1
+        a, b = (a+1)//2, (b+1)//2
+    return answer
