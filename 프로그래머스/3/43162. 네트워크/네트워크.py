e852a81f27cfbebf26ba166a2822dcd04bf5a34a
HaNeul <moho000498@gmail.com>
2024-04-26 23:31:29 +0900

    [level 3] Title: 네트워크, Time: 0.27 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,25 +1,25 @@
 def solution(n, computers):
+    # n : 노드 수
+    # computers : 연결 정보 2차원 배열
     answer = 0
     graph = [[] for _ in range(n+1)]
-    
-    for i in range(1,n+1):
-        for j in range(1,i):
-            if i==j:
-                continue
-            if computers[i-1][j-1]==1:
-                graph[i].append(j)
-                graph[j].append(i)
-    
-    def DFS(node):
-        visited[node] = True
-        for next in graph[node]:
-            if not visited[next]:
-                DFS(next)
+    for i in range(len(computers)):
+        for j in range(len(computers[i])): 
+            if computers[i][j]==1: 
+                graph[i+1].append(j+1)
                 
+    def DFS(node):
+        # 방문처리
+        visited[node] = True
+        for x in graph[node]:
+            # 아직 방문 안했으면 고
+            if visited[x]==False:
+                DFS(x)
+    
     visited = [False for _ in range(n+1)]
     
     for i in range(1, n+1):
-        if not visited[i]:
+        if visited[i]==False:
             DFS(i)
             answer +=1
     
