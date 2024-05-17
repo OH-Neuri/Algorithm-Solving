c897b9947b517cf33666221026facd0aa6ca2296
HaNeul <moho000498@gmail.com>
2024-05-17 23:50:24 +0900

    [level 2] Title: 피로도, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=javascript) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.12 ms
+메모리: 33.5 MB, 시간: 0.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 31일 22:17:38
+2024년 05월 17일 23:50:27
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -0,0 +1,20 @@
+function solution(k, dungeons) {
+  let answer = 0;
+  const visited = Array.from({ length: dungeons.length }, () => false);
+    
+  function DFS(hp, L) {
+      for (let i = 0; i < dungeons.length; i++) {
+          if (!visited[i] && dungeons[i][0] <= hp) {
+            visited[i] = true;
+            DFS(hp - dungeons[i][1], L + 1);
+            visited[i] = false;
+          }
+    }
+
+    answer = Math.max(answer, L);
+  }
+
+  DFS(k, 0);
+
+  return answer;
+}
