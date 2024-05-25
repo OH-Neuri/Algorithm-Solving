96a7a7855bcff8b96204827e5c52400423d1643d
HaNeul <moho000498@gmail.com>
2024-05-25 23:47:44 +0900

    [level 2] Title: 롤케이크 자르기, Time: 90.82 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -0,0 +1,23 @@
+function solution(topping) {
+	const map = new Map();
+    
+    const bro = new Set();
+   
+    let answer = 0;
+
+    for(let i=0; i<topping.length; i++){
+        map.has(topping[i]) ? map.set(topping[i], map.get(topping[i])+1) : map.set(topping[i], 1);
+    }
+    
+    for(let i=0; i<topping.length; i++){
+        let count = map.get(topping[i])-1;
+        bro.add(topping[i]);
+        
+   
+        count === 0 ? map.delete(topping[i]) : map.set(topping[i], count);
+
+        if(bro.size === map.size) answer++;
+
+    }
+    return answer;
+}
