5ad9e57477ba0918fed7b449879a70382d21e969
HaNeul <moho000498@gmail.com>
2024-06-21 23:39:28 +0900

    [level 2] Title: 프로세스, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,23 +1,20 @@
 function solution(priorities, location) {
-    let answer = 0;
-    let index = [];
-    let sortedPriorities = priorities.slice().sort((a, b) => b - a); // 우선순위를 내림차순으로 정렬한 배열
-    
-    for (let i = 0; i < priorities.length; i++) {
-        index.push(i); // 인덱스 배열 초기화
-    }
-    
-    while (index.length > 0) {
-        let curr = index.shift(); // 현재 인덱스 추출
-        if (priorities[curr] < sortedPriorities[0]) { // 현재 작업의 우선순위가 최고 우선순위보다 낮을 때
-            index.push(curr); // 다시 큐의 맨 뒤에 추가
-        } else { // 현재 작업의 우선순위가 최고 우선순위와 같거나 높을 때
-            answer += 1; // 인쇄
-            sortedPriorities.shift(); // 최고 우선순위 업데이트
-            if (curr === location) { // 요청한 문서일 경우
-                return answer; // 인쇄 순서 반환
-            }
-        }
-    }
-}
+  let answer = 0;
+  // process와 index값 저장
+  const array = priorities.map((process,index) => {
+    return {process, index};
+  })
 
+  while(array.length){
+    const queue = array.shift();
+    // some 메서드를 사용해서 queue.process 값보다 큰게 있는지 없는지 확인 있으면 push
+    if(array.some((element) => element.process > queue.process)){
+      array.push(queue);
+    }else{
+      // 없으면 answer++ index값이 location이랑 같아지면 break
+      answer++
+      if(queue.index === location) break;
+    }
+  }
+  return answer;
+}
