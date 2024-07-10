01ac5b8f90593af61381ba22059c20c269f22b28
HaNeul <moho000498@gmail.com>
2024-07-10 23:49:16 +0900

    [level 2] Title: 리코쳇 로봇, Time: 1.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
@@ -0,0 +1,52 @@
+function solution(board) {
+    var answer = 0;
+    board = board.map(items => items.split(''));
+    
+    const q = [];
+    const n =board.length;     // 가로 길이
+    const m = board[0].length; // 세로 길이
+    const dx = [-1, 1, 0, 0];  // 상하좌우 방향
+    const dy = [0, 0, -1, 1];
+    
+    board.forEach((items, i) => {
+        items.forEach((item, j) => {
+            if(item === 'R') q.push([i, j]);  // 시작 위치
+        });
+    });
+	
+    board[q[0][0]][q[0][1]] = 'O';   
+    
+    while(q.length) {
+    
+        const size = q.length;
+        
+        for(let i=0; i<size; i++) {
+            const [x, y] = q.shift();
+          
+            for(let j=0; j<4; j++) {
+            
+                let nx = x + dx[j];
+                let ny = y + dy[j];
+                
+                while(nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] !== 'D') {
+                    nx += dx[j];
+                    ny += dy[j];
+                }
+                
+                nx -= dx[j];
+                ny -= dy[j];
+                
+                if(board[nx][ny] === 'G') return answer+1;
+                
+                if(board[nx][ny] !== 'O') {
+                
+                    board[nx][ny] = 'O';
+                    q.push([nx, ny]);
+                }
+            }
+        }
+        answer++;
+    }
+    
+    return -1;
+}
