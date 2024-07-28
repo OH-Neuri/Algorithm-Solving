2d8d42178082385d0f7d96fe19e59231e9599e3c
HaNeul <moho000498@gmail.com>
2024-07-28 22:42:20 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.77 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -2,8 +2,6 @@ function solution(A,B){
     var answer = 0;
     A = A.sort((a,b)=>a-b)
     B = B.sort((a,b)=>b-a)
-    for(let i=0;i<A.length;i++){
-        answer += (A[i]*B[i])
-    }
-    return answer;
+
+    return A.reduce((total, value, idx)=> total+(value*B[idx]),0)
 }
