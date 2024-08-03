4f5429414205c9cfbb7ab228c0584d2957a4368b
HaNeul <moho000498@gmail.com>
2024-08-03 14:00:11 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.js
+++ LeetCode/1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.js
@@ -4,12 +4,16 @@
  * @return {boolean}
  */
 var canBeEqual = function(target, arr) {
-    let len = target.length;
-    let targetArr = Array.from({length:len},()=>0)
-    let arrArr = Array.from({length:len},()=>0)
-    for(let i=0;i<len;i++){
+    // let len = target.length;
+    let targetArr = Array.from({length:Math.max([...target])},()=>0)
+    let arrArr = Array.from({length:Math.max([...arr])},()=>0)
+    
+    console.log(targetArr)
+    console.log(arrArr)
+    for(let i=0;i<target.length;i++){
         targetArr[target[i]] +=1
         arrArr[arr[i]] +=1
     }
+
     return targetArr.join() == arrArr.join() ? true : false
 };
