7e2c68115b497f7fa248391fa95e034cab85fb2a
HaNeul <moho000498@gmail.com>
2024-08-08 23:11:38 +0900

    [level 3] Title: 부대복귀, Time: 269.78 ms, Memory: 183 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/README.md
+++ 프로그래머스/3/132266. 부대복귀/README.md
@@ -1,10 +1,10 @@
 # [level 3] 부대복귀 - 132266 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132266) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132266#qna) 
 
 ### 성능 요약
 
-메모리: 87.5 MB, 시간: 99.72 ms
+메모리: 183 MB, 시간: 269.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 11일 14:53:03
+2024년 08월 08일 23:11:37
 
 ### 문제 설명
 
--- 프로그래머스/3/132266. 부대복귀/부대복귀.js
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.js
@@ -1,32 +1,31 @@
 function solution(n, roads, sources, destination) {
-    let graph = Array.from({length : n + 1}, () =>[])
-    let visited = Array.from({length : n + 1}, () => -1)
-    
-    for(let [s, e] of roads) {
-        graph[e].push(s)
-        graph[s].push(e)
+    // 노드 저장
+    let graph = Array.from({length: n + 1}, () => []);
+    let costs = Array.from({length: n + 1}, () => -1); // 초기값을 -1로 설정
+
+    // 그래프 초기화
+    for (let [s, e] of roads) {
+        graph[s].push(e);
+        graph[e].push(s);
     }
-    let queue = [[destination,0]]
-    
-    while(queue.length){
-        let [curr, cnt] = queue.shift()
-        
-        // 출발지와 도착지가 같은 경우
-        if(visited[curr]===-1 && curr===destination){
-            visited[curr] = 0
+
+    // BFS 시작
+    let queue = [destination];
+    costs[destination] = 0; // 목적지의 거리는 0
+
+    let begin = 0;
+    while (begin < queue.length) {
+        let curr = queue[begin++];
+        let currDist = costs[curr];
+
+        for (let next of graph[curr]) {
+            if (costs[next] === -1) { // 아직 방문하지 않은 노드
+                costs[next] = currDist + 1;
+                queue.push(next);
+            }
         }
-        
-        for (let next of graph[curr]){
-            if(visited[next]===-1){
-                visited[next] = cnt+1
-                queue.push([next,cnt+1])
-            } 
-        }
     }
-    var answer = [];
-    for(let source of sources){
-        answer.push(visited[source])
-    }
-    return answer;
-    
+
+    // 출발 노드에 대한 결과를 반환
+    return sources.map(source => costs[source]);
 }
