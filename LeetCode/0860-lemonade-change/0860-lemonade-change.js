9d7734223a7437a517fe1ec19bf8afddf4755e1d
HaNeul <moho000498@gmail.com>
2024-08-15 16:26:43 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/0860-lemonade-change/0860-lemonade-change.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function (bills) {
+    let money = [0, 0, 0]
+    for (let bill of bills) {
+        if (bill == 5) money[0] += 1
+        if (bill == 10) {
+            if (money[0] <= 0) return false
+            money[1] += 1
+            money[0] -= 1
+        }
+        if (bill == 20) {
+            if (money[0] <= 0) return false
+            if (money[0] < 3 && money[1] < 1) return false
+            if (money[1]>1) { 
+                money[1] -= 1
+                money[0] -= 1
+             } else {
+                money[0] -=3
+            }
+        }
+    }
+    return true
+};
