5d6ba8fa5ef5a0401766e3a16ceada640d734d46
HaNeul <moho000498@gmail.com>
2024-08-17 01:02:56 +0900

    Time: 122 ms (43.75%), Space: 66.4 MB (96.88%) - LeetHub

--- LeetCode/0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.js
+++ LeetCode/0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.js
@@ -3,11 +3,16 @@
  * @return {number}
  */
 var maxDistance = function (arrays) {
-    let sortedMaxValue = Object.entries(arrays).sort((a, b) => b[1][b[1].length - 1] - a[1][a[1].length - 1])
-    let maxValue = [[sortedMaxValue[0][0], sortedMaxValue[0][1][sortedMaxValue[0][1].length - 1]], [sortedMaxValue[1][0], sortedMaxValue[1][1][sortedMaxValue[1][1].length - 1]]]
-    let sortedMinValue = Object.entries(arrays).sort((a, b) => (a[1][0]) - (b[1][0]))
-    let minValue = [[sortedMinValue[0][0], sortedMinValue[0][1][0]], [sortedMinValue[1][0], sortedMinValue[1][1][0]]]
+    let minValue = arrays[0][0]
+    let maxValue = arrays[0][arrays[0].length-1]
+    let result = 0
+    for(let i=1; i<arrays.length; i++){
+        let currentMin = arrays[i][0]
+        let currentMax = arrays[i][arrays[i].length-1]
 
-    if (maxValue[0][0] !== minValue[0][0]) return Math.abs(maxValue[0][1] - minValue[0][1])
-    return Math.max(Math.abs(maxValue[1][1] - minValue[0][1]), Math.abs(maxValue[0][1] - minValue[1][1]))
+        result = Math.max(result, Math.max(Math.abs(maxValue-currentMin), Math.abs(currentMax-minValue)))
+        maxValue = Math.max(maxValue, currentMax)
+        minValue = Math.min(minValue, currentMin)
+    }
+    return result
 };
