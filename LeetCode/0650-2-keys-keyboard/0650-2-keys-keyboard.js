f4410e7c3d04b7169c41c5fb83a96e8317999405
HaNeul <moho000498@gmail.com>
2024-08-19 23:56:20 +0900

    Time: 51 ms (85.19%), Space: 49.1 MB (69.14%) - LeetHub

--- LeetCode/0650-2-keys-keyboard/0650-2-keys-keyboard.js
+++ LeetCode/0650-2-keys-keyboard/0650-2-keys-keyboard.js
@@ -1,27 +1,16 @@
-/**
- * @param {number} n
- * @return {number}
- */
-var minSteps = function (n) {
-    if (n == 1) return 0
-    if (n == 3) return 3
-    if (n % 2 !== 0) return n + 1
+var minSteps = function(n) {
+    if (n === 1) return 0;
     
-    let result = solution(n)
-    let answer = 0
-    for (let [key, value] of result) {
-        answer += (key*value)
-    } 
-    return answer 
-}
- function solution(n) {
-  let numCnt= new Map()
-  for (var i = 2; i <= n; i++) {
-    while (n % i === 0) {
-      numCnt.set(i, (numCnt.get(i)||0)+1)
-      n = n / i;
+    let steps = 0;
+    let factor = 2;
+    
+    while (n > 1) {
+        while (n % factor === 0) {
+            steps += factor;
+            n = Math.floor(n / factor);
+        }
+        factor++;
     }
-  }
-  return numCnt
-}
-  
+    
+    return steps;
+};
