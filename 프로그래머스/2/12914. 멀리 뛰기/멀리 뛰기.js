0da7ac04808fa0c0d09439bf6c6c97285a513173
HaNeul <moho000498@gmail.com>
2024-08-25 23:41:22 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.59 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,7 +1,10 @@
 function solution(n) {
-  const dp = [0,1,2];
-  for(let i=3; i<=n; i++){
-    dp[i] = (dp[i-1] + dp[i-2])%1234567; 
-  }
-  return dp[n] 
+    const dp = Array.from({length:n}).fill(0); 
+    dp[0] = 1, dp[1] = 1; 
+
+  	for (let i=2; i<=n; i++) {
+        dp[i] = (dp[i-2] + dp[i-1])%1234567;
+    }
+    
+    return dp[n];
 }
