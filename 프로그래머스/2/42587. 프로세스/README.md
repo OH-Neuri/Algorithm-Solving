76582ee42b44737e2ef96295dae6a5c33cc9e929
HaNeul <moho000498@gmail.com>
2024-08-29 23:53:30 +0900

    [level 2] Title: 프로세스, Time: 451.38 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.21 ms
+메모리: 37 MB, 시간: 451.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 21일 23:39:27
+2024년 08월 29일 23:53:26
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,20 +1,17 @@
 function solution(priorities, location) {
-  let answer = 0;
-  // process와 index값 저장
-  const array = priorities.map((process,index) => {
-    return {process, index};
-  })
-
-  while(array.length){
-    const queue = array.shift();
-    // some 메서드를 사용해서 queue.process 값보다 큰게 있는지 없는지 확인 있으면 push
-    if(array.some((element) => element.process > queue.process)){
-      array.push(queue);
-    }else{
-      // 없으면 answer++ index값이 location이랑 같아지면 break
-      answer++
-      if(queue.index === location) break;
+    const queue = []
+    priorities = priorities.map((priority, index) => [priority, index])
+    
+    while(priorities.length) {
+        const [priority, index] = priorities.shift()
+        const higherImportance = priorities.findIndex(([value, _]) => value > priority)
+        
+        if(higherImportance === -1){
+            if(index === location) return queue.length + 1  
+            queue.push([priority, index])  
+            continue
+        }
+        
+        priorities.push([priority, index])
     }
-  }
-  return answer;
 }
