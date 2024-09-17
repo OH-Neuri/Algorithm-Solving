1d5eb02b1a91bc8b8612e043d00cadab07c4b0b6
HaNeul <moho000498@gmail.com>
2024-09-17 23:30:50 +0900

    [level 2] Title: 괄호 변환, Time: 0.43 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/README.md
+++ 프로그래머스/2/60058. 괄호 변환/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 변환 - 60058 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60058?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60058) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.29 ms
+메모리: 33.4 MB, 시간: 0.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 06일 23:46:10
+2024년 09월 17일 23:30:50
 
 ### 문제 설명
 
--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.js
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.js
@@ -1,17 +1,49 @@
-function reverse(str) {
-  return str.slice(1, str.length - 1).split("").map((c) => (c === "(" ? ")" : "(")).join("");
+function solution(p) {
+    var answer = '';
+    var open = 0;
+    var close = 0;
+    
+    // 1. 재귀 탈출 조건
+    if (!p) return ''; 
+    
+    for (let i=0; i<p.length; i++) {
+        if (p[i] === '(') open++
+        else close++
+        
+        // 균형일 때 "올바른 괄호 문자열"인지 아닌지 판단
+        if (open === close) {
+            // 2, 3, 3-1
+            if (check(p.slice(0, i+1))) {
+                answer = p.slice(0, i+1) + solution(p.slice(i+1));
+                return answer;
+            } else {
+                // 4-1, 4-2, 4-3
+                answer = '(' + solution(p.slice(i+1)) + ')';
+                
+                // 4-4
+                for (let j=1; j<i; j++) {
+                    if (p[j] === '(') {
+                        answer = answer + ')'
+                    } else {
+                        answer = answer + '('
+                    }
+                }
+                // 4-5
+                return answer;
+            }
+        }
+    }    
 }
 
-function solution(p) {
-  if (p.length < 1) return "";
-
-  let balance = 0;
-  let pivot = 0;
-  do { balance += p[pivot++] === "(" ? 1 : -1 } while (balance !== 0);
-
-  const u = p.slice(0, pivot);
-  const v = solution(p.slice(pivot, p.length));
-
-  if (u[0] === "(" && u[u.length - 1] == ")") return u + v;
-  else return "(" + v + ")" + reverse(u);
+// 올바른 괄호 문자열인지 판단하는 함수
+function check(p) {
+    var stack = [];
+    for (let i = 0; i<p.length; i++) {
+        if (p[i] === '(') stack.push('(')
+        else {
+            if (stack.length === 0) return false;
+            stack.pop();
+        }
+    }
+    return true;
 }
