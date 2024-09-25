f24d40dd118caa35aeda9091f548cf1c2017a0b3
HaNeul <moho000498@gmail.com>
2024-09-25 23:49:04 +0900

    [level 2] Title: 소수 찾기, Time: 4.30 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,45 +1,33 @@
 function solution(numbers) {
-  const answer = new Set();
-
-  for (let i = 1; i <= numbers.length; i++) {
-    const permutation = [...getPermutation([...numbers], i)];
-    const primeNumbers = permutation.filter((arr) => {
-      const number = +arr.join("");
-      const isPrimeNumber = checkPrimeNumber(number);
-      return isPrimeNumber;
-    });
-
-    primeNumbers.forEach((arr) => {
-      answer.add(+arr.join(""));
-    });
-  }
-  return answer.size;
+    return [...new Set(getPer(numbers))].filter(v => isPrime(v)).length;
 }
 
-const checkPrimeNumber = (number) => {
-  if (number < 2) return false;
+const getPer = (str) => {
+    const answer = [];
+    const n = str.length;
+    let ch = Array.from({ length: n }, () => 0);
+    
+    const dfs = (curStr) => {
+        answer.push(+curStr);
+        for (let i = 0; i < n; i++) {
+            if (ch[i] === 0) {
+                ch[i] = 1;
+                dfs(curStr + str[i]);
+                ch[i] = 0;
+            }
+        }
+    }
+    dfs('');
+    answer.shift();
+    return answer;
+}
 
-  for (let i = 2; i <= Math.sqrt(number); i++) {
-    const remainder = number % i;
-    if (remainder === 0) return false;
-  }
-  return true;
-};
-
-const getPermutation = (arr, selectNumber) => {
-  const results = [];
-  if (selectNumber === 1) return arr.map((v) => [v]);
-  else {
-    arr.forEach((fixed, index, origin) => {
-      const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
-      const permutations = getPermutation(rest, selectNumber - 1);
-      const attached = permutations.map((permutation) => [
-        fixed,
-        ...permutation,
-      ]);
-      results.push(...attached);
-    });
-  }
-
-  return results;
-};
+const isPrime = (n) => {
+    if (n === 0 || n === 1) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
