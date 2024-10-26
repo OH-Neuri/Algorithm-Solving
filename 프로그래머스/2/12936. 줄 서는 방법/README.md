7d899cf9e93a22c659383c360944219d02dfbe7c
HaNeul <moho000498@gmail.com>
2024-10-26 23:44:27 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/README.md
+++ 프로그래머스/2/12936. 줄 서는 방법/README.md
@@ -1,10 +1,10 @@
 # [level 2] 줄 서는 방법 - 12936 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12936?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12936) 
 
 ### 성능 요약
 
-메모리: 33 MB, 시간: 0.20 ms
+메모리: 33.4 MB, 시간: 0.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 04일 23:16:02
+2024년 10월 26일 23:44:28
 
 ### 문제 설명
 
--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
@@ -1,29 +1,13 @@
-function solution (n, k) {
-  const answer = [];
-  const arr = new Array(n).fill(1);
-  for(let i = 1; i < n; i++) arr[i] = arr[i-1] + 1;
-  
-  let nth = k-1;
-  
-  while(arr.length) {
-    if(nth === 0) {
-      answer.push(...arr);
-      break;
+function solution(n, k) {
+    const answer = [];
+    const people = Array.from({ length: n }, (_, i) => i + 1);
+    let caseNum = people.reduce((ac, v) => ac * v, 1)
+    
+    while (answer.length < n) {
+        caseNum = caseNum / people.length;
+        answer.push(...people.splice(Math.floor((k - 1) / caseNum), 1));
+        k = k % caseNum;
     }
-    
-    const fact = factorial(arr.length - 1);
-    const index = nth / fact >> 0;
-    nth = nth % fact;
-    
-    answer.push(arr[index]);
-    arr.splice(index, 1);
-  }
-  
-  return answer;
-}
 
-const factorial = (n) => {
-  let res = 1;
-  for(let i = 2; i <= n; i++) res *= i;
-  return res;
+    return answer;
 }
