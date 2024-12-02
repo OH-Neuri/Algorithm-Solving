7b63ebedc2d00f0fed58c05c2c9c97d0e7ba071e
HaNeul <moho000498@gmail.com>
2024-12-02 23:47:30 +0900

    [level 3] Title: 네트워크, Time: 0.53 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,20 +1,33 @@
 function solution(n, computers) {
-    let visited = Array.from({length:n}, ()=>false)
-    var answer = 0;
-    for(let i=0;i<n;i++){
-       if(!visited[i]){
-           answer +=1
-           DFS(i)
-       } 
-    }
-    function DFS(node){
-        visited[node] = true
-        
-        computers[node].forEach((n, i)=>{
-            if(n==1 && !visited[i]) DFS(i)
-        })
-}
-    
-    return answer;
-}
+    // 네트워크 수 세기
+    let networks = 0;
 
+    // 방문 여부 확인하기
+    let visit = Array(n).fill(false);
+    
+    // 방문한 곳인지 확인 및 네트워크 수 증가시키기
+    for (let i = 0; i < n; i++) {
+        if (!visit[i]) {
+            networks ++;
+            isVisit(i);
+        }
+    }
+    
+    // DFS를 통해 방문한 곳인지 확인하는 함수
+    function isVisit(index) {
+        visit[index] = true;
+        
+        const computer = computers[index];
+        
+        for (let i = 0; i < computer.length; i++) {
+            const isConnect = computer[i] === 1 ? true : false;
+            
+            // 방문하지 않았고 && 연결된 곳인지 확인
+            if (!visit[i] && isConnect) {
+                isVisit(i);
+            }
+        }        
+    }
+    
+    return networks;
+}
