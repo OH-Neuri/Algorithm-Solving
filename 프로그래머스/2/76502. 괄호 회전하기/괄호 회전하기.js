7d8d46b98f9de74d21b2adb251b37420917f77ed
HaNeul <moho000498@gmail.com>
2024-12-31 23:09:09 +0900

    [level 2] Title: 괄호 회전하기, Time: 26.31 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,32 +1,31 @@
-
 function solution(s) {
-    var answer = 0;
-    let list = check(s)
-    const regex1 = /(\(\))/g
-    const regex2 = /(\{\})/g
-    const regex3 = /(\[\])/g
-    for(let i = 0; i<list.length;i++){
-        let num=0
-        while(list[i].match(regex1)||list[i].match(regex2)||list[i].match(regex3)){
-
-           list[i]= list[i].replace(regex1, '').replace(regex2, '').replace(regex3, '');
-        }
-             
-        if(list[i].length===0){
-            answer++
-        }
-    }
+    let answer = 0;
+    let stack = [];
+    let isCorrect = true;
+  
+    if (s.length % 2 === 1) return 0;   //주어진 문자열이 홀수인 경우에는 바로 0을 리턴
+    
+    for(let i=0; i<s.length; i++){       
+       let str = s.slice(i) + s.slice(0,i);   //문자열을 왼쪽으로 1칸씩 회전
+       isCorrect = true;
+      
+        for(let n of str){
+            if(n === "[" || n === "{" || n === "(" ){
+                stack.push(n);   //[ or { or ( 이면 stack으로 push
+            }else{
+                let opening = stack.pop();   //stack에 쌓여있는 요소중 마지막
+                //↓ 짝이 맞으면 continue
+                if (opening === "(" && n === ")") continue;
+                if (opening === "{" && n === "}") continue;
+                if (opening === "[" && n === "]") continue;
+                //↓ 짝이 맞지 않으면 실패, isCorrect를 false로 만들고 break
+              	isCorrect = false;
+                break;
+            };
+        };
+      
+        //짝이 맞는다면 성공이므로 isCorrect는 true, answer에 +1씩 더하기
+        if (isCorrect) answer++;
+    };
     return answer;
-}
-function check (s){
-    s = s.split('')
-    const list = [];
-    const num = s.length;
-    for(let i =0; i<num;i++){
-        
-    let temp = s.shift();
-    s.push(temp)
-    list.push(s.join(''))
-    }
-    return list
-}
+};
