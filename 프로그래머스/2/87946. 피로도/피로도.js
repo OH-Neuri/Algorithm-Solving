e2a0ae8c7fd84bb2ed59645c71ef96de096d7a3c
HaNeul <moho000498@gmail.com>
2025-01-06 23:25:25 +0900

    [level 2] Title: 피로도, Time: 5.76 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,18 +1,18 @@
 function solution(k, dungeons) {
-  let answer = 0;
-  const visited = Array.from({ length: dungeons.length }, () => false);
-  function DFS(hp, L) {
-    for (let i = 0; i < dungeons.length; i++) {
-      if (!visited[i] && dungeons[i][0] <= hp) {
-        visited[i] = true;
-        DFS(hp - dungeons[i][1], L + 1);
-        visited[i] = false;
-      }
+    let answer = 0
+    let visited = new Array(dungeons.length).fill(false)
+    
+    const DFS = (hp , count) => {
+        for(let i = 0 ; i < dungeons.length ; i++) {
+            if(!visited[i] && hp >= dungeons[i][0]){
+                visited[i] = true
+                DFS(hp - dungeons[i][1] , count + 1)
+                visited[i] = false
+            }    
+        }
+        answer = Math.max(answer, count)
     }
-    answer = Math.max(answer, L);
-  }
-
-  DFS(k, 0);
-
-  return answer;
+    
+    DFS(k,0)
+    return answer
 }
