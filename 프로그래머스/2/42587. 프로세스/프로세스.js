f7a79c2230df57c854ecffee2423193841252f5e
HaNeul <moho000498@gmail.com>
2025-01-22 23:49:50 +0900

    [level 2] Title: 프로세스, Time: 0.39 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,17 +1,25 @@
 function solution(priorities, location) {
-    const queue = []
-    priorities = priorities.map((priority, index) => [priority, index])
-    
-    while(priorities.length) {
-        const [priority, index] = priorities.shift()
-        const higherImportance = priorities.findIndex(([value, _]) => value > priority)
+    let answer = 0;
+    let arr = []
+    let max_value = Math.max(...priorities);
+
+    //위치 배열 만들기
+    for(let i = 0; i < priorities.length; i++){
+        arr.push(i);
+    }
+
+   //priorities 배열이 비어있을 때까지 반복
+    while(priorities.length != 0){
+         max_value = Math.max(...priorities);
         
-        if(higherImportance === -1){
-            if(index === location) return queue.length + 1  
-            queue.push([priority, index])  
-            continue
+        if(priorities[0] < max_value){
+            priorities.push(priorities.shift());
+            arr.push(arr.shift());
+        }else {
+            answer+=1;
+            priorities.shift();
+            if(arr.shift() == location)
+                return answer;
         }
-        
-        priorities.push([priority, index])
     }
 }
