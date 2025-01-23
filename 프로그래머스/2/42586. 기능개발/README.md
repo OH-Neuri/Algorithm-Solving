d756da16ee25bf5956ab99f844392114abca04a9
HaNeul <moho000498@gmail.com>
2025-01-23 23:47:24 +0900

    [level 2] Title: 기능개발, Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.05 ms
+메모리: 33.4 MB, 시간: 0.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 22일 00:15:58
+2025년 01월 23일 23:47:22
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,25 +1,21 @@
 function solution(progresses, speeds) {
-    var answer = [];
-    let days = 1;
+  let answer = [];
+
+  while (speeds.length > 0) { // speed 배열길이가 0이 될때까지
     let cnt = 0;
-    let progress = 0;
-    
-    while(progresses[0]){
-        progress = progresses[0] + (speeds[0] * days);
-        if(progress >= 100){
-            cnt++;
-            progresses.shift();
-            speeds.shift();
-        }
-        else{
-            if(cnt > 0){
-                answer.push(cnt);
-            }
-            days++;
-            cnt = 0;
-        }
+    for (let i = 0; i < speeds.length; i++) { // progress와 speed 짝지어 더하기
+      if (progresses[i] < 100) { // 100이 넘어가면 그만 더하기
+        progresses[i] += speeds[i];
+      }
     }
-    answer.push(cnt);
-    
-    return answer;
+    while (progresses[0] >= 100) { // 맨앞의 progress배열이 100이 넘으면 shift
+      progresses.shift();
+      speeds.shift(); // speed도 shift
+      cnt++;
+    }
+    if (cnt > 0) {
+      answer.push(cnt);
+    }
+  }
+  return answer;
 }
