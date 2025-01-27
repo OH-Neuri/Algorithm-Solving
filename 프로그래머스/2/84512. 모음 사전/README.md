297895ffe3de5eb2e7fb970ff9f943dea41e9c20
HaNeul <moho000498@gmail.com>
2025-01-27 23:52:15 +0900

    [level 2] Title: 모음 사전, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 33.1 MB, 시간: 0.06 ms
+메모리: 33.4 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 01일 23:47:22
+2025년 01월 27일 23:52:15
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,11 +1,11 @@
 function solution(word) {
-  let answer = 0;
-  const vowels = ["A", "E", "I", "O", "U"];
-  const counts = [781, 156, 31, 6, 1];
-
-  for (let i = 0; i < word.length; i++) {
-    answer += vowels.indexOf(word[i]) * counts[i] + 1;
-  }
-
-  return answer;
+    const vowels = ['A', 'E', 'I', 'O', 'U'];
+    const counts = [781, 156, 31, 6, 1]; // 각 자리수별 가능한 단어 수
+    
+    let answer = 0;
+    for (let i = 0; i < word.length; i++) {
+        answer += vowels.indexOf(word[i]) * counts[i] + 1; // 해당 자리의 알파벳 인덱스에 따른 영향을 더해줌
+    }
+    
+    return answer;
 }
