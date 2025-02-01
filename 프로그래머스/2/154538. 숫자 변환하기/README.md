f36ea2ef157392af5d6d28800bfa803ea6f581be
HaNeul <moho000498@gmail.com>
2025-02-01 23:26:19 +0900

    [level 2] Title: 숫자 변환하기, Time: 2511.90 ms, Memory: 68.7 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/README.md
+++ 프로그래머스/2/154538. 숫자 변환하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자 변환하기 - 154538 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154538?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154538) 
 
 ### 성능 요약
 
-메모리: 49.2 MB, 시간: 50.41 ms
+메모리: 68.7 MB, 시간: 2511.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 24일 23:41:29
+2025년 02월 01일 23:26:17
 
 ### 문제 설명
 
--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,24 +1,12 @@
 function solution(x, y, n) {
- 
-    if(x===y){
-        return 0;
-    }else if(x>y){
-        return -1;
+    var answer = 0;
+    var que = [[y, 0]];
+    while(que.length) {
+        const [num, i] = que.shift();
+        if(num === x) return i;
+        if(num%2 === 0 && num/2 >= x) que.push([num/2, i+1]);
+        if(num%3 === 0 && num/3 >= x) que.push([num/3, i+1]);
+        if(num-n >= x) que.push([num-n, i+1]);
     }
-    
-    let countArray = Array(y+1).fill(Infinity)
-    countArray[x] = 0;
-    
-    for(i=x+1; i<y+1; ++i){
-        if(i-n>=x){
-            countArray[i] = Math.min(countArray[i],countArray[i-n]+1)
-        }
-        if(i%2===0&&i/2>=x){
-            countArray[i] = Math.min(countArray[i],countArray[i/2]+1)         
-        }
-        if(i%3===0&&i/3>=x){
-            countArray[i] = Math.min(countArray[i],countArray[i/3]+1)         
-        }
-    }
-    return countArray[y]===Infinity? -1:countArray[y]
+    return -1;
 }
