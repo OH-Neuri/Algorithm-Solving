72ddb220cc41c8a10aed61cedfdd301e09e5c47f
HaNeul <moho000498@gmail.com>
2025-02-12 22:08:22 +0900

    [level 2] Title: 큰 수 만들기, Time: 36.35 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,17 +1,11 @@
 function solution(number, k) {
-  let stack = [];
-
-  for (let i = 0; i < number.length; i++) {
-    const element = number[i];
-    // stack 마지막 숫자와 현재 숫자를 비교하여 현재 숫자가 더 크면 stack의 마지막 숫자 pop
-    while (k > 0 && stack[stack.length - 1] < element) {
-      stack.pop();
-      k--;
+  const answer = [];
+  for (let num of number) {
+    while (k > 0 && answer[answer.length - 1] < num) {
+      answer.pop(num);
+      k -= 1;
     }
-    stack.push(element);
+    answer.push(num);
   }
-
-  // 모든 숫자 비교가 끝난 후, k가 0보다 크다면 남은 k만큼 뒤에서 제거
-  stack.splice(stack.length - k, k);
-  return stack.join("");
+  return answer.join("").slice(0, number.length - k);
 }
