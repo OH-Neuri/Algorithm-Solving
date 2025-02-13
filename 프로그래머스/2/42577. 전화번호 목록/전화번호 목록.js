a9eabda53180052eb6470e53ec3cf422ff0ba3a6
HaNeul <moho000498@gmail.com>
2025-02-13 23:50:23 +0900

    [level 2] Title: 전화번호 목록, Time: 152.00 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,12 +1,11 @@
 function solution(phone_book) {
-  phone_book.sort(); 
+  phone_book.sort(); // 전화번호 리스트를 정렬합니다.
 
   for (let i = 0; i < phone_book.length - 1; i++) {
-    
     if (phone_book[i] === phone_book[i + 1].substring(0, phone_book[i].length)) {
       return false;
     }
   }
 
-  return true; 
+  return true;
 }
