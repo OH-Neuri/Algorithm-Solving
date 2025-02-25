c69762ce84b4a46585472c4c342d26cff93f8501
HaNeul <moho000498@gmail.com>
2025-02-25 23:45:57 +0900

    [level 2] Title: 삼각 달팽이, Time: 2525.55 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/README.md
+++ 프로그래머스/2/68645. 삼각 달팽이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 삼각 달팽이 - 68645 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645) 
 
 ### 성능 요약
 
-메모리: 103 MB, 시간: 34.93 ms
+메모리: 112 MB, 시간: 2525.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 27일 23:41:01
+2025년 02월 25일 23:45:54
 
 ### 문제 설명
 
--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -1,37 +1,36 @@
-const solution = (n) => {
-  const answer = [];
-  const arr = Array.from({ length: n }, () => new Array(n).fill(0));
-
-  const directions = [
-    [1, 0],
-    [0, 1],
-    [-1, -1],
-  ];
-
-  let row = -1;
-  let col = 0;
-
-  let nowValue = 1;
-  let nowDirectionIndex = 0;
-
-  for (let i = n; i > 0; i -= 1) {
-    const [dRow, dCol] = directions[nowDirectionIndex];
-
-    for (let j = 0; j < i; j += 1) {
-      row += dRow;
-      col += dCol;
-
-      arr[row][col] = nowValue;
-      nowValue += 1;
+function solution(n) {
+  let answer = [];
+  let count = 0;
+  let arr = Array.from({ length: n }, (_, index) => Array(index + 1).fill(0));
+  let currentX = -1;
+  let currentY = 0;
+  while (n > 0) {
+    // n의 값이 유효할 때까지 반복
+    for (let i = 0; i < n; i++) {
+      // 위에서 아래로 내려가는 대각선
+      currentX++;
+      count++;
+      arr[currentX][currentY] = count;
     }
-
-    nowDirectionIndex = (nowDirectionIndex + 1) % 3;
+    for (let i = 0; i < n - 1; i++) {
+      // 왼쪽에서 오른쪽으로 이동
+      currentY++;
+      count++;
+      arr[currentX][currentY] = count;
+    }
+    for (let i = 0; i < n - 2; i++) {
+      // 아래에서 위로 내려가는 대각선
+      currentX--;
+      currentY--;
+      count++;
+      arr[currentX][currentY] = count;
+    }
+    n -= 3;
   }
 
-  for (let i = 0; i < n; i += 1) {
-    for (let j = 0; j < n; j += 1) {
-      if (arr[i][j]) answer.push(arr[i][j]);
-    }
+  for (let i = 0; i < arr.length; i++) {
+    answer = [...answer, ...arr[i]];
   }
+
   return answer;
-};
+}
