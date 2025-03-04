5deaf3aac94e127757adca2630d9ae75639493e3
HaNeul <moho000498@gmail.com>
2025-03-04 23:19:48 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.27 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 38.8 MB, 시간: 13.29 ms
+메모리: 38.1 MB, 시간: 11.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 23일 00:00:04
+2025년 03월 04일 23:19:45
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,32 +1,27 @@
 function solution(maps) {
-    var n = maps.length; // 행
-    var m = maps[0].length; // 열
-    var dir = [[-1, 0], [1, 0], [0, -1], [0, 1]]; // 상하좌우
-
-    // bfs
-    var bfs = () => {
-        var queue = [[0, 0, 1]]; // 행, 열, 이동거리
-        maps[0][0] = 0; // 이동한 거리 0으로 막기
-
-        while (queue.length > 0) {  //큐가 비어있지 않는 동안 반복
-            var [row, col, distance] = queue.shift();
-
-            if (row == n - 1 && col == m - 1) {
-                return distance;
-            }
-
-            for (var [r, c] of dir) {
-                var newRow = row + r;
-                var newCol = col + c;   //현재 위치에서 상하좌우로 이동한 새로운 위치
-
-                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && maps[newRow][newCol] === 1) {
-                    queue.push([newRow, newCol, distance + 1]);
-                    maps[newRow][newCol] = 0; // 방문한 위치를 0으로 표시
+    if(maps===[1]){return 1;}
+    var visit = [];
+    var queue = [[0,0]];
+    var moveX = [-1,1,0,0];
+    var moveY = [0,0,-1,1];
+    var count =1;
+    if(maps[maps.length - 1][maps[0].length - 2] === 0 && maps[maps.length - 2][maps[0].length - 1] === 0) return -1;
+    while(queue.length>0){
+        var queLen = queue.length;
+        for(var q = 0; q<queLen;q++){
+            let [x,y] = queue.shift();
+            for(var i =0;i<4;i++){
+                var newX = x + moveX[i];
+                var newY = y + moveY[i];
+                if(newX===maps[0].length-1&&newY===maps.length-1) return ++count;
+                if(newX>=0&&newY>=0&&newX<maps[0].length&&newY<maps.length&&maps[newY][newX]===1){
+                    queue.push([newX,newY]);
+                    maps[newY][newX] = 0;
                 }
             }
         }
-        return -1;
-    };
-
-    return bfs();
+        count++;
+    }
+    
+    return -1;
 }
