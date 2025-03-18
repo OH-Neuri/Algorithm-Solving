c0ed50da269c804477cb7f63ea3b271eb591a945
HaNeul <moho000498@gmail.com>
2025-03-18 21:15:09 +0900

    [level 2] Title: 후보키, Time: 7.75 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.js
+++ 프로그래머스/2/42890. 후보키/후보키.js
@@ -1,50 +1,56 @@
 function solution(relation) {
-  const col = relation[0].length;
-  const arr = Array.from(Array(relation[0].length), (_, i) => i);
-  let comb = [];
-  for (let i = 0; i < col; i++) {
-    comb.push(...combination(arr, i + 1));
-  }
-
-  comb = checkUnique(relation, comb);
-  comb = checkMinimal(comb);
-  return comb.length;
+    const colLen = relation[0].length;
+    const columnArr = Array.from({length:colLen}, (_,i)=>i);
+    let candidateKeys = [];
+    
+    for(let r = 1; r <= colLen; r++){
+        const combinations = getCombination(columnArr,r)
+        for(let candidate of combinations){
+            if(isUnique(relation, candidate) && isMinimal(candidate, candidateKeys)){
+                candidateKeys.push(candidate)
+            }
+        }
+    }
+    
+    return candidateKeys.length;
 }
 
-function combination(arr, selectNum) {
-  if (selectNum === 1) return arr.map((v) => [v]);
-
-  const result = [];
-  arr.forEach((fix, i, origin) => {
-    const combi = combination(origin.slice(i + 1), selectNum - 1);
-    const attach = combi.map((c) => [fix, ...c]);
-    result.push(...attach);
-  });
-  return result;
+// 최소성 확인
+function isMinimal(candidate, selectedKeys){
+    for(let key of selectedKeys){
+        if(key.every(col => candidate.includes(col))) return false
+    }
+    return true
 }
 
-function checkUnique(relation, comb) {
-  let result = [];
-
-  comb.forEach((c) => {
-    let set = new Set();
-    relation.forEach((rel) => set.add(c.map((e) => rel[e]).join(',')));
-    if (set.size == relation.length) result.push(c);
-  });
-
-  return result;
+// 유일성 확인
+function isUnique(relation, columns){
+    const seen = new Set();
+    for(let row of relation){
+        const key = columns.map(col => row[col]).join(",");
+        if(seen.has(key)) return false;
+        seen.add(key)
+    }
+    return true;
 }
 
-function checkMinimal(comb) {
-  let result = [];
 
-  while (comb.length) {
-    result.push(comb[0]);
-    comb = comb.reduce((a, c) => {
-      if (!comb[0].every((e) => c.includes(e))) a.push(c);
-      return a;
-    }, []);
-  }
-
-  return result;
+function getCombination(arr, selectNumber){
+    const result = []
+    const n = arr.length;
+    
+    function dfs(start, path){
+        if(path.length=== selectNumber){
+            result.push([...path]);
+            return 
+        }
+        
+        for(let i = start; i<n; i++){
+            path.push(arr[i]);
+            dfs(i+1, path);
+            path.pop()
+        }
+    }
+    dfs(0,[]);
+    return result 
 }
