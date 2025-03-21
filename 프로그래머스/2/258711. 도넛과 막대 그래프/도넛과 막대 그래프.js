922a6fc18da69d8c1d0a1b5f7a73034bb6054d4c
HaNeul <moho000498@gmail.com>
2025-03-21 16:50:19 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 786.50 ms, Memory: 256 MB -BaekjoonHub

---
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.js
@@ -0,0 +1,52 @@
+function solution(edges) {
+    const N = 1_000_001;
+    const inDeg = Array(N).fill(0);
+    const outDeg = Array(N).fill(0);
+    const graph = Array.from({ length: N }, () => []);
+
+    for (const [from, to] of edges) {
+        graph[from].push(to);
+        outDeg[from]++;
+        inDeg[to]++;
+    }
+
+    let start = 0;
+    for (let i = 0; i < N; i++) {
+        if (inDeg[i] === 0 && outDeg[i] >= 2) {
+            start = i;
+            break;
+        }
+    }
+
+    let donut = 0, stick = 0, eight = 0;
+
+    for (const next of graph[start]) {
+        let node = next;
+        const visited = new Set();
+
+        while (true) {
+            if (visited.has(node)) {
+                // 사이클이면서, 분기 없는 도넛
+                donut++;
+                break;
+            }
+            visited.add(node);
+
+            if (outDeg[node] === 0) {
+                // 막대
+                stick++;
+                break;
+            }
+
+            if (outDeg[node] >= 2) {
+                // 중간에 분기점 있음 → 8자
+                eight++;
+                break;
+            }
+
+            node = graph[node][0]; // 다음 노드 (무조건 하나뿐이거나 undefined)
+        }
+    }
+
+    return [start, donut, stick, eight];
+}
