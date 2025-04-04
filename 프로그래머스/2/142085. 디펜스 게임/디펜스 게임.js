cccbfda9f0c7692bc34971f09ebe63ad0d48df39
HaNeul <moho000498@gmail.com>
2025-04-04 20:39:25 +0900

    [level 2] Title: 디펜스 게임, Time: 131.17 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.js
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.js
@@ -1,64 +1,87 @@
-class PriorityQueue {
-  constructor() {
-      this.heap = [];
-  }
-  
-  push(value) {
-      const heap = this.heap;
-      heap.push(value);
-      let index = heap.length - 1;
-      let parent = Math.floor((index - 1) / 2);
-      
-      while (index > 0 && heap[index] < heap[parent]) {
-          [heap[index], heap[parent]] = [heap[parent], heap[index]];
-          index = parent;
-          parent = Math.floor((index - 1) / 2);
-      }
-  }
-  
-  pop() {
-      const heap = this.heap;
-      if (heap.length <= 1) {
-          return heap.pop();
-      }
-      
-      const output = heap[0];
-      heap[0] = heap.pop();
-      let index = 0;
-      
-      while (index * 2 + 1 < heap.length) {
-          let left = index * 2 + 1, right = index * 2 + 2, next = index;
-          if (heap[next] > heap[left]) {
-              next = left;
-          }
-          if (right < heap.length && heap[next] > heap[right]) {
-              next = right;
-          }
-          if (next === index) {
-              break;
-          }
-          [heap[index], heap[next]] = [heap[next], heap[index]];
-          index = next;
-      }
-      
-      return output;
-  }
+// 우선순위 큐 구현해야해.............................................................T-T
+// 정렬 갈겨 -> 시간초과
+function solution(n, k, enemy) {
+    var heap = new MinHeap()
+    
+    for(let i = 0; i < enemy.length; i++){
+        heap.push(enemy[i])
+        
+        if(heap.size() > k){
+             n -= heap.pop()
+        }
+        
+        if(n < 0) return i
+    }
+    
+    return enemy.length;
 }
 
-function solution(n, k, enemy) {
-  
-  let arr = new PriorityQueue();
-  var capacity = 0;
-
-  enemy.slice(0,k).forEach((element)=>arr.push(element));
-  for(var i  =k;i<enemy.length;i++){
-    arr.push(enemy[i]);
-    var popNum = arr.pop();
-    if(popNum+capacity>n){
-      return i;
+class MinHeap {
+    constructor() {
+        this.heap = [];
     }
-    capacity+=popNum;
-  }
 
-  return enemy.length;
+    push(val) {
+        this.heap.push(val);
+        this.bubbleUp();
+    }
+
+    pop() {
+        const min = this.heap[0];
+        const end = this.heap.pop();
+        if (this.heap.length > 0) {
+            this.heap[0] = end;
+            this.sinkDown();
+        }
+        return min;
+    }
+
+    size() {
+        return this.heap.length;
+    }
+
+    bubbleUp() {
+        let idx = this.heap.length - 1;
+        const element = this.heap[idx];
+        while (idx > 0) {
+            const parentIdx = Math.floor((idx - 1) / 2);
+            const parent = this.heap[parentIdx];
+            if (element >= parent) break;
+            this.heap[parentIdx] = element;
+            this.heap[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+
+    sinkDown() {
+        let idx = 0;
+        const length = this.heap.length;
+        const element = this.heap[0];
+
+        while (true) {
+            let leftIdx = 2 * idx + 1;
+            let rightIdx = 2 * idx + 2;
+            let swap = null;
+
+            if (leftIdx < length) {
+                if (this.heap[leftIdx] < element) {
+                    swap = leftIdx;
+                }
+            }
+
+            if (rightIdx < length) {
+                if (
+                    (swap === null && this.heap[rightIdx] < element) ||
+                    (swap !== null && this.heap[rightIdx] < this.heap[leftIdx])
+                ) {
+                    swap = rightIdx;
+                }
+            }
+
+            if (swap === null) break;
+            this.heap[idx] = this.heap[swap];
+            this.heap[swap] = element;
+            idx = swap;
+        }
+    }
 }
