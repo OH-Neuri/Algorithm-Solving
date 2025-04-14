0f154d6515de419f1f083b9a0911d4cee608927a
HaNeul <moho000498@gmail.com>
2025-04-14 21:44:00 +0900

    [level 2] Title: [1차] 캐시, Time: 34.77 ms, Memory: 42.9 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.51 ms
+메모리: 42.9 MB, 시간: 34.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 29일 23:30:34
+2025년 04월 14일 21:44:00
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,24 +1,30 @@
 function solution(cacheSize, cities) {
-    const city = cities.map(a => a.toLowerCase()); 
-    // 배열 내 요소 소문자로
+    const cache = []
+    var answer = 0;
     
-    let queue = [];
-    let time = 0;
-    
-    for(let i = 0; i < city.length; i++){
-        if(!queue.includes(city[i])){ // 큐에 도시가 존재하지 않으면
-            time+=5;
-            queue.push(city[i]);
-            if(queue.length > cacheSize){ // 캐시사이즈를 넘으면 앞 요소 삭제
-                queue.shift();
+    for(let city of cities){
+        // 대소문자 구분 없음
+        city = city.toLowerCase() 
+        
+        const idx = cache.indexOf(city)
+        
+        // cache hit
+        if(idx!==-1){
+            cache.splice(idx,1) // 해당 도시 제거
+            cache.push(city) // 뒤에 추가
+            answer+=1            
+        }else{ 
+        // cache miss
+            // cache에 빈 공간이 없는 경우,
+            if(cacheSize && cache.length >= cacheSize){
+                cache.shift() // 가장 오래된 도시 제거
             }
-        }
-        else{ // 도시가 큐에 있으면
-            time++;
-            let idx = queue.indexOf(city[i]); 
-            queue.splice(idx, 1);
-            queue.push(city[i]); // 인덱스 찾아서 제거 후 맨 뒤에 삽입
+            
+            // cahce에 도시 넣기
+            if(cacheSize) cache.push(city)
+            answer += 5            
         }
     }
-    return time;
+    
+    return answer;
 }
